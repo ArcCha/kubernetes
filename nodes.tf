173d51cdbcc822ee2cedb6c7bccb01944293ad47
Adam Piekarczyk <adam.piekarczyk@softnet.com.pl>
2020-01-23 12:14:06 +0100

    Add nodes

--- nodes.tf
+++ nodes.tf
@@ -1,14 +1,14 @@
 resource "libvirt_domain" "master" {
-  name    = "kubernetes-master"
-  memory  = "1024"
-  vcpu    = 1
+  name   = "kubernetes-master"
+  memory = "1024"
+  vcpu   = 1
 
   cloudinit = libvirt_cloudinit_disk.commoninit.id
 
   network_interface {
-    network_id      = libvirt_network.k8s.id 
-    hostname        = "master"
-    wait_for_lease  = true
+    network_id     = libvirt_network.k8s.id
+    hostname       = "master"
+    wait_for_lease = true
   }
 
   console {
@@ -24,6 +24,33 @@ resource "libvirt_domain" "master" {
   }
 
   disk {
-    volume_id   = libvirt_volume.master.id
+    volume_id = libvirt_volume.master.id
   }
+}
+
+resource "libvirt_domain" "node" {
+  count  = 2
+  name   = "node-${count.index}"
+  memory = "1024"
+  vcpu   = 1
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_id     = libvirt_network.k8s.id
+    hostname       = "node-${count.index}"
+    wait_for_lease = true
+  }
+
+  disk {
+    volume_id = libvirt_volume.node["${count.index}"].id
+  }
+}
+
+output "master_ip" {
+  value = libvirt_domain.master.network_interface.0.addresses
+}
+
+output "node_ips" {
+  value = libvirt_domain.node.*.network_interface.0.addresses
 }
--- volumes.tf
+++ volumes.tf
@@ -1,23 +1,30 @@
 resource "libvirt_pool" "kubernetes" {
-  name    = "kubernetes"
-  type    = "dir"
-  path    = "/home/ap2/Work/Kubernetes/images"
+  name = "kubernetes"
+  type = "dir"
+  path = "/home/ap2/Work/Kubernetes/images"
 }
 
 resource "libvirt_volume" "centos7_cloud" {
-  name    = "centos7_cloud.qcow2"
-  pool    = libvirt_pool.kubernetes.name
-  source  = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  name   = "centos7_cloud.qcow2"
+  pool   = libvirt_pool.kubernetes.name
+  source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
   name      = "commoninit.iso"
   pool      = libvirt_pool.kubernetes.name
-  user_data = file("${path.module}/templates/cloud_init.tmpl") 
+  user_data = file("${path.module}/templates/cloud_init.tmpl")
 }
 
 resource "libvirt_volume" "master" {
-  name            = "master.qcow2"
-  pool            = "kubernetes"
-  base_volume_id  = libvirt_volume.centos7_cloud.id
+  name           = "master.qcow2"
+  pool           = "kubernetes"
+  base_volume_id = libvirt_volume.centos7_cloud.id
+}
+
+resource "libvirt_volume" "node" {
+  count          = 2
+  name           = "node-${count.index}.qcow2"
+  pool           = "kubernetes"
+  base_volume_id = libvirt_volume.centos7_cloud.id
 }
